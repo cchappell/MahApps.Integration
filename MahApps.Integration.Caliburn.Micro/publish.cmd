a3c775f8b075f3acff32ddbe1179f446549ded7c
cchappell <c_chappell5@hotmail.com>
2019-01-08 19:44:11 -0600

    Remove publish cmd.

--- MahApps.Integration.Caliburn.Micro/publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget pack MahApps.Integration.Caliburn.Micro.csproj -Build -Prop Configuration=Release
-nuget push MahApps.Integration.Caliburn.Micro.1.1.2.nupkg -source "E:\Users\Chris\OneDrive\nuget"
-pause
